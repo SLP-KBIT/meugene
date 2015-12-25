18e98c2aa48e395827ef8040aed6bb6a807a59f4
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2015-12-26 05:20:42 +0900

    [modify] show and some

---
+++ app/controllers/meugenes_controller.rb
@@ -0,0 +1,13 @@
+class MeugenesController < ApplicationController
+
+  def index
+  end
+  def create
+    session[:text] = params[:input_text]
+    redirect_to meugene_path(1)
+  end
+  def show
+    @text = session[:text]
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :meugene
+  resources :meugenes
+  root 'meugenes#index'
+  # post 'meugene/index' => 'meugene#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
