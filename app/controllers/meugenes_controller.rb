2451d42ea9e9013255a297595fefb64df859335e
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-01-23 03:46:12 +0900

    [modify] 入力フォームの常時表示

--- app/controllers/meugenes_controller.rb
+++ app/controllers/meugenes_controller.rb
@@ -2,16 +2,27 @@ class MeugenesController < ApplicationController
 
   def index
   end
+
   def create
-    @text = Text.create(text_params)
+    @text = Text.new(text_params)
     @input = text_params[:input_text]
-    @text[:meu_text] = @text.change_to_meu(@input)
-    @text.save
-    redirect_to meugene_path(1)
+    unless @input.blank?
+      @meu = @text.change_to_meu(@input)
+      @text[:meu_text] = @meu
+      @text.save
+      redirect_to meugene_path(id: @text) and return   # show にセーブしたモデルのid を渡す
+    end
+    render 'index'
   end
+
   def show
-    @input_text = Text.last[:input_text]
-    @meu_text = Text.last[:meu_text]
+    unless params[:id].blank?
+      @text_id = params[:id]
+      @text = Text.new
+      @text = Text.find_by id: @text_id
+      @input_text = @text[:input_text]
+      @meu_text = @text[:meu_text]
+    end
   end
 
   private
